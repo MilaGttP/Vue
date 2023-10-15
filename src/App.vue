ece5673cab134b08dade12ff65b44265ea2c83d6
MilaGttP <milaivanova2609@gmail.com>
2023-10-15 16:49:21 +0300

    Completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  
+  <div class="App">
+    <Timer name="Europe/Kiev" />
+    <Timer name="America/New_York" />
+    <Timer name="Asia/Hong_Kong" />
+  </div>
 </template>
 
 <script>
+import Timer from './сomponents/timer.vue';
 
+export default {
+  components: {
+    Timer
+  }
+};
 </script>
-
-<style>
-
-</style>
