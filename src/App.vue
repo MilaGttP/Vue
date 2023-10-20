a70008a6187528be541446b06cd1c7d9344d32fa
MilaGttP <milaivanova2609@gmail.com>
2023-10-20 12:55:35 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
-  <div class="App">
-    <Timer v-for="(timer, index) in timers" :key="index" :name="timer.name" />
-  </div>
+  
 </template>
 
 <script>
-import Timer from './components/clock.vue';
-import axios from 'axios';
-
 export default {
-  components: {
-    Timer
-  },
-  data() {
-    return {
-      timers: []
-    };
-  },
-  methods: {
-    loadTimerData() {
-      axios.get('/api/timers')
-        .then(response => {
-          this.timers = response.data;
-        })
-        .catch(error => {
-          console.error('Помилка завантаження даних про таймери:', error);
-        });
-    }
-  },
-  created() {
-    this.loadTimerData();
-  }
-};
+
+}
 </script>
+
+<style>
+
+</style>
